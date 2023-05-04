9a4fa2d8d8b689393004000eb02e381900750788
odedev <odedev@outlook.com>
2023-05-04 10:08:20 +0800

    update

--- fleet-ui/packages/common/vite.config.ts
+++ fleet-ui/packages/common/vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 23503
+    port: 23502
   },
   plugins: [vue()],
   resolve: {
