3d907da60a2c5eb2bf740a267272dafced519546
odedev <odedev@outlook.com>
2023-05-08 22:51:30 +0800

    update

--- fleet-develop/src/main/java/dev/odes/fleet/develop/entity/ModelField.java
+++ fleet-develop/src/main/java/dev/odes/fleet/develop/entity/ModelField.java
@@ -1,8 +1,21 @@
 package dev.odes.fleet.develop.entity;
 
-import dev.odes.fleet.common.field.Field;
+import dev.odes.fleet.common.enumeration.DataTypeEnum;
 
-public class ModelField extends Field {
+public class ModelField {
     private String id;
+    private String code;
+    private String name;
+    private DataTypeEnum dataType;
+    private Integer length;
+    private String defaultValue;
+    private String enumeration;
+    private String entity;
+    private Boolean isNullable;
+    private Boolean isSearchable;
+    private Boolean isDefaultDisplay;
+    private Boolean isMasterField;
+    private Boolean isSlaveField;
+    private String model;
 
 }
