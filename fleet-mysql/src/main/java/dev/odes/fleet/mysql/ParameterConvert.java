91a7afc9bc18c2790faee1996d395c0ec5973b96
odedev <odedev@outlook.com>
2023-05-14 22:08:42 +0800

    update

--- fleet-mysql/src/main/java/dev/odes/fleet/mysql/ParameterConvert.java
+++ fleet-mysql/src/main/java/dev/odes/fleet/mysql/ParameterConvert.java
@@ -6,24 +6,19 @@ import dev.odes.fleet.common.parameter.ParameterProperty;
 
 import java.util.List;
 
-/**
- * package: dev.odes.fleet.mysql
- * class: ParameterConvert
- * date: 2023/5/7 0:43
- * version: 1.0.0
- * description:
- */
 public class ParameterConvert {
     public static String toWhere(Parameter parameter) {
-        StringBuilder where = new StringBuilder();
+        StringBuilder where = new StringBuilder("1=1");
         List<ParameterProperty> parameterProperties = parameter.getProperties();
         parameterProperties.forEach(parameterProperty -> {
             String key = parameterProperty.getKey();
             Object value = parameterProperty.getValue();
             DataTypeEnum dataType = parameterProperty.getDataType();
-            if (!where.isEmpty()) {
-                where.append(" AND ");
+
+            if (value == null) {
+                return;
             }
+            where.append(" AND ");
             if (dataType == DataTypeEnum.MODEL) {
                 where.append(key);
                 where.append("=");
