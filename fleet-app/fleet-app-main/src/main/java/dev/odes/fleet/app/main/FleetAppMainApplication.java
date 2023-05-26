d3d3745c3d9742364f23d59e9da2da3ae41fd974
odedev <odedev@outlook.com>
2023-05-26 18:03:22 +0800

    update

--- fleet-app/fleet-app-main/src/main/java/dev/odes/fleet/app/main/FleetAppMainApplication.java
+++ fleet-app/fleet-app-main/src/main/java/dev/odes/fleet/app/main/FleetAppMainApplication.java
@@ -1,9 +1,13 @@
 package dev.odes.fleet.app.main;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"dev.odes.fleet.develop.**.mapper", "dev.odes.fleet.core.**.mapper"})
+@ComponentScan(basePackages = {"dev.odes.fleet.core.**", "dev.odes.fleet.develop.*"})
 public class FleetAppMainApplication {
 
 	public static void main(String[] args) {
--- fleet-core/fleet-core-file/src/main/java/dev/odes/fleet/core/file/config/WebFileConfig.java
+++ fleet-core/fleet-core-file/src/main/java/dev/odes/fleet/core/file/config/WebFileConfig.java
@@ -1,8 +1,12 @@
 package dev.odes.fleet.core.file.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 @Configuration
 public class WebFileConfig implements WebMvcConfigurer {
@@ -10,7 +14,16 @@ public class WebFileConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         WebMvcConfigurer.super.addResourceHandlers(registry);
+
+        File file;
+        try {
+            file = ResourceUtils.getFile("public/");
+        } catch (FileNotFoundException e) {
+            file = new File("/public/");
+        }
+        System.out.println("source-----" + file.getAbsolutePath());
+
         registry.addResourceHandler("/public/**")
-            .addResourceLocations("file://./public");
+            .addResourceLocations("file://" + file.getAbsolutePath() + "/");
     }
 }
