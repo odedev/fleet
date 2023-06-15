8aaa565d0a93863c129c47e6f0af18c5bcdb70a0
odedev <odedev@outlook.com>
2023-06-15 22:52:06 +0800

    update

--- fleet-ui/packages/common/src/components/editor/FlEditorRichText.vue
+++ fleet-ui/packages/common/src/components/editor/FlEditorRichText.vue
@@ -1,12 +1,108 @@
 <template>
-<Editor :editor="ClassicEditor"></Editor>
+<Editor :model-value="value" :editor="ClassicEditor" :config="config" @input="handleInput" tag-name="textarea"></Editor>
 </template>
 
 <script setup lang="ts">
+import {ref, watch} from "vue";
 import CKEditor from '@ckeditor/ckeditor5-vue';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import { EditorUI } from '@ckeditor/ckeditor5-ui';
+import { Heading, HeadingButtonsUI } from '@ckeditor/ckeditor5-heading';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { Link, LinkImage } from '@ckeditor/ckeditor5-link';
+import { Paragraph, ParagraphButtonUI } from '@ckeditor/ckeditor5-paragraph';
+import { Font } from '@ckeditor/ckeditor5-font';
+import { Table, TableColumnResize, TableCellProperties, TableProperties, TableToolbar, TableCaption } from '@ckeditor/ckeditor5-table';
+import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
+import { Highlight } from '@ckeditor/ckeditor5-highlight';
+import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
+import { Image, ImageCaption, ImageResize, ImageStyle, ImageToolbar } from '@ckeditor/ckeditor5-image';
+import { Undo } from '@ckeditor/ckeditor5-undo';
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Style } from '@ckeditor/ckeditor5-style';
+import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
 
 const Editor = CKEditor.component;
+
+const config = ref({
+  plugins: [
+    EditorUI,
+    Heading,
+    HeadingButtonsUI,
+    Essentials,
+    Table,
+    TableToolbar,
+    TableColumnResize,
+    TableCellProperties,
+    TableProperties,
+    TableCaption,
+    Bold,
+    Italic,
+    Font,
+    Link,
+    Alignment,
+    Paragraph,
+    ParagraphButtonUI,
+    CodeBlock,
+    Highlight,
+    Indent,
+    IndentBlock,
+    HorizontalLine,
+    Image, ImageToolbar, ImageCaption, ImageStyle, ImageResize, LinkImage,
+    Undo,
+    Style,
+    GeneralHtmlSupport,
+  ],
+  toolbar: [
+    'undo',
+    'redo',
+    'heading',
+    'bold',
+    'italic',
+    'fontSize',
+    'fontFamily',
+    'fontColor',
+    'fontBackgroundColor',
+    'alignment',
+    '|',
+    'insertTable',
+    'link',
+    'highlight',
+    'codeBlock',
+    'outdent',
+    'indent',
+    'horizontalLine',
+    'imageStyle:block',
+    'imageStyle:side',
+    '|',
+    'toggleImageCaption',
+    'imageTextAlternative',
+    '|',
+    'linkImage',
+    'style',
+  ],
+  table: {
+    contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells', 'toggleTableCaption' ]
+  },
+  style: {
+    definitions: [
+      // Styles definitions.
+      // ...
+    ]
+  },
+
+})
+
+const value = ref('')
+value.value = '<p>Content of the editor.</p>'
+
+
+const handleInput = () => {
+  console.log(value)
+};
 
 </script>
 
@@ -26,9 +122,13 @@ const Editor = CKEditor.component;
 }
 .ck-rounded-corners .ck.ck-editor__main>.ck-editor__editable,
 .ck.ck-editor__main>.ck-editor__editable.ck-rounded-corners {
-  @include border-color-primary();
+  //@include border-color-primary();
 
   box-shadow: none;
+}
+
+.ck.ck-editor__editable.ck-focused:not(.ck-editor__nested-editable) {
+  @include border-color-primary();
 }
 
 </style>
--- fleet-ui/packages/common/vite.config.ts
+++ fleet-ui/packages/common/vite.config.ts
@@ -2,13 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 23503
   },
-  plugins: [vue()],
+  plugins: [vue(), ckeditor5({theme: require.resolve( '@ckeditor/ckeditor5-theme-lark' )})],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
