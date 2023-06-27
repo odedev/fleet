0e5a4a54899b6c736cee76622e9fae50c6e722a7
odedev <odedev@outlook.com>
2023-06-27 23:00:46 +0800

    update

--- fleet-tool/src/module/router.js
+++ fleet-tool/src/module/router.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/user', function(req, res, next) {
+  res.render('system/user', { title: 'Fleet user' });
+});
+
 router.get('/setting', function(req, res, next) {
   res.render('system/setting', { title: 'Fleet setting' });
 });
