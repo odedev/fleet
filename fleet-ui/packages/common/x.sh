f3054e1e2a23634b8977294eb990e138fe90ba0b
odedev <odedev@outlook.com>
2023-09-18 17:38:42 +0800

    update

--- fleet-ui/packages/common/x.sh
+++ fleet-ui/packages/common/x.sh
@@ -1,6 +1,10 @@
 
-npm install --save-dev vite vue-tsc @vue/tsconfig
+npm install --save vue vue-router pinia
+npm install --save @arco-design/web-vue
+npm install --save axios qs socket.io-client dayjs
 
+
+npm install --save-dev vite vue-tsc @vue/tsconfig
 npm install --save-dev prettier eslint eslint-plugin-vue
 npm install --save-dev @rushstack/eslint-patch npm-run-all
 npm install --save-dev @tsconfig/node18 @types/node
