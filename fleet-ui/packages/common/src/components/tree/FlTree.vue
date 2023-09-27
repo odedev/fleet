96f4a9c8687513741a4ea22c9a6a1f0921433af2
odedev <odedev@outlook.com>
2023-09-27 18:08:47 +0800

    update

--- fleet-ui/packages/common/src/components/tree/FlTree.vue
+++ fleet-ui/packages/common/src/components/tree/FlTree.vue
@@ -8,7 +8,6 @@
       <Tree
         v-model:selected-keys="selectedKeys"
         :data="treeData"
-        size="small"
         :selectable="true"
         :multiple="false"
         :checkable="false"
@@ -16,41 +15,142 @@
         :block-node="true"
         :draggable="true"
         :show-line="false"
+        :field-names="fieldNames"
+        size="small"
       >
+        <template #icon="node">
+          <i>
+            <component :is="node.icon" />
+          </i>
+        </template>
       </Tree>
     </div>
   </section>
 </template>
 <script lang="ts" setup>
-import {ref} from "vue";
-import {Tree, InputSearch} from '@arco-design/web-vue'
+import {ref, h} from "vue";
+import {Tree, InputSearch, } from '@arco-design/web-vue'
 import "@arco-design/web-vue/es/tree/style/index.css";
 import "@arco-design/web-vue/es/tree-select/style/index.css";
 import "@arco-design/web-vue/es/input/style/index.css";
 
+import { IconApps, IconCommon, IconLayout, IconRelation, IconUnorderedList } from '@arco-design/web-vue/es/icon';
+
+import type {TreeFieldNames} from '@arco-design/web-vue'
+
+interface Props {
+  modelValue: boolean,
+  disabled?: boolean,
+  checkable?: boolean,
+  selectionType?: 'none' | 'single' | 'multiple',
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  disabled: false,
+});
+
+const emits = defineEmits(['click']);
+
 const selectedKeys = ref([]);
 const multiple = ref(true);
+
+// 指定节点数据中的字段名
+const fieldNames = ref<TreeFieldNames>({
+  key: 'id',
+  title: 'title',
+  children: 'children',
+  icon: 'icon',
+  disabled: '',
+  isLeaf: '',
+  disableCheckbox: '',
+  checkable: '',
+});
+
 const defaultTreeData = [
   {
     title: '用户中心',
-    key: '0-0',
+    id: '0-0',
     children: [
       {
-        title: '用户',
-        key: '0-0-1',
-      },
-      {
-        title: 'Branch 0-0-2',
-        key: '0-0-2',
+        icon: () => h(IconApps),
+        draggable: false,
+        title: '应用',
+        id: '0-0-11',
         children: [
           {
             title: 'Leaf',
-            key: '0-0-2-1'
+            id: '0-0-1-1'
           },
           {
             draggable: false,
             title: 'Leaf 0-0-2-2 (Drag disabled)',
-            key: '0-0-2-2'
+            id: '0-0-1-2'
+          }
+        ]
+      },
+      {
+        icon: () => h(IconCommon),
+        draggable: false,
+        title: '模型',
+        id: '0-0-2',
+        children: [
+          {
+            title: 'Leaf',
+            id: '0-0-2-1'
+          },
+          {
+            title: 'Leaf 0-0-2-2 (Drag disabled)',
+            id: '0-0-2-2'
+          }
+        ]
+      },
+      {
+        icon: () => h(IconUnorderedList),
+        draggable: false,
+        title: '枚举',
+        id: '0-0-3',
+        children: [
+          {
+            title: 'Leaf',
+            id: '0-0-3-1'
+          },
+          {
+            draggable: false,
+            title: 'Leaf 0-0-2-2 (Drag disabled)',
+            id: '0-0-3-2'
+          }
+        ]
+      },
+      {
+        icon: () => h(IconRelation),
+        draggable: false,
+        title: '接口',
+        id: '0-0-21',
+        children: [
+          {
+            title: 'Leaf',
+            id: '0-0-21-1'
+          },
+          {
+            title: 'Leaf 0-0-2-2 (Drag disabled)',
+            id: '0-0-21-2'
+          }
+        ]
+      },
+      {
+        icon: () => h(IconLayout),
+        draggable: false,
+        title: '视图',
+        id: '0-0-4',
+        children: [
+          {
+            title: 'Leaf',
+            id: '0-0-4-1'
+          },
+          {
+            draggable: false,
+            title: 'Leaf 0-0-2-2 (Drag disabled)',
+            id: '0-0-4-2'
           }
         ]
       },
@@ -58,25 +158,25 @@ const defaultTreeData = [
   },
   {
     title: '系统管理',
-    key: '0-1',
+    id: '0-1',
     children: [
       {
         title: 'Branch 0-1-1',
-        key: '0-1-1',
+        id: '0-1-1',
         children: [
           {
             title: 'Leaf',
-            key: '0-1-1-1',
+            id: '0-1-1-1',
           },
           {
             title: 'Leaf',
-            key: '0-1-1-2',
+            id: '0-1-1-2',
           },
         ]
       },
       {
         title: 'Leaf',
-        key: '0-1-2',
+        id: '0-1-2',
       },
     ],
   },
@@ -150,6 +250,9 @@ const onDrop = ({ dragNode, dropNode, dropPosition }) => {
   }
   .arco-tree-node-switcher {
     height: 28px;
+  }
+  .arco-tree-node-icon {
+    margin-right: 8px;
   }
 }
 </style>
