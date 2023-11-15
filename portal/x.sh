f8d2fec11e1e4b3a79f1466d143e13b58ec5da5a
odedev <odedev@outlook.com>
2023-11-15 10:56:14 +0800

    update

--- portal/x.sh
+++ portal/x.sh
@@ -1,5 +1,7 @@
 
 npm install --save @element-plus/icons-vue element-plus
 
-npm install --save-dev @element-plus/nuxt @nuxt/devtools @nuxtjs/tailwindcss @types/node
-npm install --save-dev autoprefixer nuxt postcss sass tailwindcss
+npm install --save-dev nuxt vue vue-router
+npm install --save-dev @nuxt/devtools @types/node
+npm install --save-dev autoprefixer postcss sass tailwindcss
+npm install --save-dev @element-plus/nuxt @nuxtjs/tailwindcss
