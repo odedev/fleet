bd5f6746650f6ff086953e4669b7e34be8cf299f
odedev <odedev@outlook.com>
2023-11-15 14:50:12 +0800

    update

---
+++ initialization/main.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+
+
+console.log(chalk.yellow('Hello world!'));
+console.log(chalk.green('Hello world!'));
+
+console.log(chalk.red('Hello world!'));
+
---
+++ initialization/x.sh
@@ -0,0 +1,10 @@
+
+
+npm install --save mysql2
+npm install --save mongodb
+npm install --save glob
+npm install --save fs-extra
+
+npm install --save chalk
+
+npm install --save-dev @types/node
