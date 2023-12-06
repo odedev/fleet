58c763e6f4f3a4becd24e21824b8c731f200d01e
odedev <odedev@outlook.com>
2023-12-06 10:19:32 +0800

    update

--- fleet-ui/packages/common/x.sh
+++ fleet-ui/packages/common/x.sh
@@ -17,7 +17,7 @@ npm install --save @codemirror/lang-javascript @codemirror/lang-html @codemirror
 npm install --save-dev vite vue-tsc @vue/tsconfig
 npm install --save-dev typescript
 npm install --save-dev prettier eslint eslint-plugin-vue
-npm install --save-dev @rushstack/eslint-patch npm-run-all
+npm install --save-dev @rushstack/eslint-patch npm-run-all npm-run-all2
 npm install --save-dev @tsconfig/node18 @types/node
 npm install --save-dev @vitejs/plugin-vue @vitejs/plugin-vue-jsx
 npm install --save-dev @vue/eslint-config-prettier @vue/eslint-config-typescript
