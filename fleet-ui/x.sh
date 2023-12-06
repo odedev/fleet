a64acd105155822695b1c05f1cedd0544388e468
odedev <odedev@outlook.com>
2023-12-06 10:46:34 +0800

    update

---
+++ fleet-ui/x.sh
@@ -0,0 +1,33 @@
+
+cd packages
+
+cd common
+npm install --save vue vue-router pinia
+npm install --save @arco-design/web-vue
+npm install --save axios qs ws socket.io-client dayjs
+npm install --save mitt crypto-js nprogress
+npm install --save vue-draggable-plus
+npm install --save @vue/repl
+
+npm install --save @bytemd/plugin-breaks @bytemd/plugin-gemoji @bytemd/plugin-gfm @bytemd/plugin-highlight
+npm install --save @bytemd/plugin-medium-zoom @bytemd/plugin-mermaid @bytemd/vue-next
+
+npm install --save @codemirror/state @codemirror/view
+npm install --save @codemirror/search @codemirror/commands @codemirror/autocomplete @codemirror/lint
+npm install --save @codemirror/language @codemirror/lang-markdown @codemirror/lang-json @codemirror/lang-sql
+npm install --save @codemirror/lang-javascript @codemirror/lang-html @codemirror/lang-css @codemirror/language-data
+
+npm install --save-dev vite vue-tsc @vue/tsconfig
+npm install --save-dev typescript
+npm install --save-dev prettier eslint eslint-plugin-vue
+npm install --save-dev @rushstack/eslint-patch npm-run-all npm-run-all2
+npm install --save-dev @tsconfig/node18 @types/node
+npm install --save-dev @vitejs/plugin-vue @vitejs/plugin-vue-jsx
+npm install --save-dev @vue/eslint-config-prettier @vue/eslint-config-typescript
+npm install --save-dev @ckeditor/vite-plugin-ckeditor5
+npm install --save-dev sass less
+
+cd ..
+
+
+cd ..
