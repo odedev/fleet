140bd1189fdb27cb3016a68445f853424058ed36
odedev <odedev@outlook.com>
2023-12-06 16:40:58 +0800

    update

---
+++ fleet-component/fleet-component-mongodb/src/main/java/dev/odes/fleet/component/mongodb/ParameterConvert.java
@@ -0,0 +1,12 @@
+package dev.odes.fleet.component.mongodb;
+
+import dev.odes.fleet.common.parameter.Parameter;
+import org.springframework.data.mongodb.core.query.Criteria;
+
+public class ParameterConvert {
+    public static String toWhere(Parameter parameter) {
+        Criteria criteria = new Criteria();
+
+        return null;
+    }
+}
