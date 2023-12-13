6a5db5989f112b1a692ec22443336cc891a3c34f
odedev <odedev@outlook.com>
2023-12-13 16:21:53 +0800

    update

---
+++ fleet-ui/dependencies/x.sh
@@ -0,0 +1,142 @@
+# 20231213
+
+npm install --save mitt
+npm install --save axios
+npm install --save qs
+npm install --save ws
+npm install --save dayjs
+npm install --save crypto-js
+npm install --save nprogress
+npm install --save lodash
+npm install --save lodash-es
+npm install --save bytes
+
+npm install --save @ctrl/tinycolor
+npm install --save bignumber.js
+npm install --save change-case
+npm install --save clipboard
+npm install --save color
+npm install --save cropperjs
+npm install --save highlight.js
+npm install --save interactjs
+npm install --save mime
+npm install --save validator
+
+npm install --save https://cdn.sheetjs.com/xlsx-0.20.1/xlsx-0.20.1.tgz
+npm install --save file-saver
+# npm install --save socket.io-client
+# npm install --save query-string
+
+
+
+npm install --save video.js
+npm install --save viewerjs
+npm install --save xgplayer
+npm install --save xgplayer-flv
+npm install --save bpmn-js
+npm install --save dhtmlx-gantt
+
+
+npm install --save vue
+npm install --save vue-router
+npm install --save pinia
+
+npm install --save @arco-design/web-vue
+npm install --save vue-draggable-plus
+npm install --save @vue/repl
+npm install --save @vueuse/core
+npm install --save vue-i18n
+
+# npm install --save echarts
+# npm install --save vue-echarts
+
+
+npm install --save sortablejs
+
+npm install --save bytemd
+npm install --save @bytemd/plugin-breaks
+npm install --save @bytemd/plugin-gemoji
+npm install --save @bytemd/plugin-gfm
+npm install --save @bytemd/plugin-highlight
+npm install --save @bytemd/plugin-medium-zoom
+npm install --save @bytemd/plugin-mermaid
+npm install --save @bytemd/vue-next
+npm install --save github-markdown-css
+
+
+npm install --save @codemirror/state
+npm install --save @codemirror/view
+npm install --save @codemirror/search
+npm install --save @codemirror/commands
+npm install --save @codemirror/autocomplete
+npm install --save @codemirror/lint
+npm install --save @codemirror/language
+npm install --save @codemirror/lang-markdown
+npm install --save @codemirror/lang-json
+npm install --save @codemirror/lang-sql
+npm install --save @codemirror/lang-javascript
+npm install --save @codemirror/lang-html
+npm install --save @codemirror/lang-css
+npm install --save @codemirror/language-data
+
+npm install --save monaco-editor
+
+npm install --save @ckeditor/ckeditor5-alignment
+npm install --save @ckeditor/ckeditor5-autoformat
+npm install --save @ckeditor/ckeditor5-basic-styles
+npm install --save @ckeditor/ckeditor5-block-quote
+npm install --save @ckeditor/ckeditor5-build-classic
+npm install --save @ckeditor/ckeditor5-clipboard
+npm install --save @ckeditor/ckeditor5-code-block
+npm install --save @ckeditor/ckeditor5-find-and-replace
+npm install --save @ckeditor/ckeditor5-font
+npm install --save @ckeditor/ckeditor5-heading
+npm install --save @ckeditor/ckeditor5-highlight
+npm install --save @ckeditor/ckeditor5-horizontal-line
+npm install --save @ckeditor/ckeditor5-html-embed
+npm install --save @ckeditor/ckeditor5-html-support
+npm install --save @ckeditor/ckeditor5-image
+npm install --save @ckeditor/ckeditor5-indent
+npm install --save @ckeditor/ckeditor5-language
+npm install --save @ckeditor/ckeditor5-list
+npm install --save @ckeditor/ckeditor5-markdown-gfm
+npm install --save @ckeditor/ckeditor5-media-embed
+npm install --save @ckeditor/ckeditor5-page-break
+npm install --save @ckeditor/ckeditor5-remove-format
+npm install --save @ckeditor/ckeditor5-restricted-editing
+npm install --save @ckeditor/ckeditor5-show-blocks
+npm install --save @ckeditor/ckeditor5-source-editing
+npm install --save @ckeditor/ckeditor5-style
+npm install --save @ckeditor/ckeditor5-table
+npm install --save @ckeditor/ckeditor5-theme-lark
+npm install --save @ckeditor/ckeditor5-undo
+npm install --save @ckeditor/ckeditor5-vue
+
+
+npm install --save-dev vite
+npm install --save-dev vue-tsc
+npm install --save-dev @vue/tsconfig
+npm install --save-dev typescript
+npm install --save-dev prettier
+npm install --save-dev eslint
+npm install --save-dev eslint-plugin-vue
+npm install --save-dev @rushstack/eslint-patch
+# npm install --save-dev npm-run-all
+npm install --save-dev npm-run-all2
+npm install --save-dev @tsconfig/node18
+npm install --save-dev @vitejs/plugin-vue
+npm install --save-dev @vitejs/plugin-vue-jsx
+npm install --save-dev @vue/eslint-config-prettier
+npm install --save-dev @vue/eslint-config-typescript
+npm install --save-dev @ckeditor/vite-plugin-ckeditor5
+npm install --save-dev sass
+npm install --save-dev less
+
+npm install --save-dev @types/node
+npm install --save-dev @types/bytes
+npm install --save-dev @types/crypto-js
+npm install --save-dev @types/mime-types
+npm install --save-dev @types/nprogress
+npm install --save-dev @types/qs
+npm install --save-dev @interactjs/types
+
