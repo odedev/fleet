a1651a3995c54c40c19ac3ee3d4f1c721db1c1e2
odedev <odedev@outlook.com>
2023-12-14 16:16:15 +0800

    update

--- fleet-ui/apps/app-dev/vite.config.ts
+++ fleet-ui/apps/app-dev/vite.config.ts
@@ -8,6 +8,28 @@ export default defineConfig({
   server: {
     port: 23521,
     host: '0.0.0.0',
+    proxy: {
+      // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
+      '/api': {
+        target: 'http://localhost:23510',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      // with RegEx: http://localhost:5173/fallback/ -> http://jsonplaceholder.typicode.com/
+      '^/api/.*': {
+        target: 'http://localhost:23510',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, options) => {
+          // proxy will be an instance of 'http-proxy'
+        },
+      },
+      // Proxying websockets or socket.io: ws://localhost:23520/socket.io -> ws://localhost:23510/socket
+      '/socket': {
+        target: 'ws://localhost:23510',
+        ws: true,
+      },
+    },
   },
   plugins: [
     vue(),
