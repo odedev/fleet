0b6f4de895d676903a5fe1a16b30f2d8bc6b47bb
odedev <odedev@outlook.com>
2024-01-03 11:48:09 +0800

    update

--- tool/x.sh
+++ tool/x.sh
@@ -1,17 +1,53 @@
 
-npm install --save express express-validator handlebars hbs helmet
-npm install --save connect-rid connect-timeout cors
-npm install --save axios change-case compression
-npm install --save dotenv http-errors http-proxy-middleware
-npm install --save mime-types morgan multer mysql2
-npm install --save nanoid nodemailer picocolors puppeteer
-npm install --save reflect-metadata response-time serve-favicon sqlite3 typeorm
-npm install --save uuid wait-on ws
-npm install --save @playwright/test
+npm install --save express
+npm install --save hbs
+npm install --save helmet
+npm install --save connect-rid
+npm install --save connect-timeout
+npm install --save cors
+npm install --save compression
+npm install --save response-time
+npm install --save serve-favicon
+npm install --save morgan
+npm install --save multer
+npm install --save http-proxy-middleware
+npm install --save express-validator
+
+npm install --save http-errors
 npm install --save jsonwebtoken
+
+npm install --save reflect-metadata
+npm install --save typeorm
+npm install --save sqlite3
+npm install --save mysql2
 
 npm install --save lowdb
 
+npm install --save nodemailer
+npm install --save picocolors
+
+npm install --save handlebars
+
+npm install --save axios
+npm install --save change-case
+npm install --save mime-types
+npm install --save uuid
+npm install --save nanoid
+
+npm install --save wait-on
+npm install --save ws
+
+npm install --save puppeteer
+npm install --save @playwright/test
+
+npm install --save dotenv
+
 # npm install --save protobufjs
 
-npm install --save-dev @types/express @types/node daisyui nodemon tailwindcss
+npm install --save-dev @types/express
+npm install --save-dev @types/node
+npm install --save-dev nodemon
+npm install --save-dev tailwindcss
+npm install --save-dev postcss
+npm install --save-dev autoprefixer
+npm install --save-dev daisyui
