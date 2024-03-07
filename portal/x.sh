1f06d42817a815c0b62595313b6047d9921e05d1
odedev <odedev@outlook.com>
2024-03-07 18:03:37 +0800

    update

--- portal/x.sh
+++ portal/x.sh
@@ -2,7 +2,10 @@
 npm install --save @arco-design/web-vue
 npm install --save @element-plus/icons-vue element-plus
 
-npm install --save-dev nuxt vue vue-router
-npm install --save-dev @nuxt/devtools @types/node
+npm install --save-dev nuxt
+npm install --save-dev vue
+npm install --save-dev vue-router
+# npm install --save-dev @nuxt/devtools
+npm install --save-dev @types/node
 npm install --save-dev autoprefixer postcss sass tailwindcss
 npm install --save-dev @element-plus/nuxt @nuxtjs/tailwindcss
