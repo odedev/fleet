98c63e5fd4777995cbf16f4c606cf86602dd40dd
odedev <odedev@outlook.com>
2024-03-07 18:47:06 +0800

    update

--- fleet-ui/apps/app-dev/vite.config.ts
+++ fleet-ui/apps/app-dev/vite.config.ts
@@ -2,37 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 23521,
-    host: '0.0.0.0',
-    proxy: {
-      // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
-      '/api': {
-        target: 'http://localhost:23510',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      // with RegEx: http://localhost:5173/fallback/ -> http://jsonplaceholder.typicode.com/
-      '^/api/.*': {
-        target: 'http://localhost:23510',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        configure: (proxy, options) => {
-          // proxy will be an instance of 'http-proxy'
-        },
-      },
-      // Proxying websockets or socket.io: ws://localhost:23520/socket.io -> ws://localhost:23510/socket
-      '/socket': {
-        target: 'ws://localhost:23510',
-        ws: true,
-      },
-    },
-  },
   plugins: [
     vue(),
+    vueJsx(),
   ],
   resolve: {
     alias: {
