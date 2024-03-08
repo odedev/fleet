e62c9cbdb0db163b7c8f85d4d4dce6808a1fcba6
odedev <odedev@outlook.com>
2024-03-08 12:01:05 +0800

    update

--- fleet-ui/apps/app-dev/vite.config.ts
+++ fleet-ui/apps/app-dev/vite.config.ts
@@ -1,14 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: '', // Hash Router
+  base: '/', // Web Router
   plugins: [
     vue(),
     vueJsx(),
+    splitVendorChunkPlugin(),
   ],
   resolve: {
     alias: {
--- fleet-ui/apps/app-legacy/vite.config.ts
+++ fleet-ui/apps/app-legacy/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,10 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    legacy({
+      targets: ['ie >= 11', "Firefox >= 50", 'Chrome >= 50', 'Edge >= 80'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    }),
     splitVendorChunkPlugin(),
   ],
   resolve: {
@@ -19,7 +24,7 @@ export default defineConfig({
     }
   },
   server: {
-    port: 23520,
+    port: 23522,
     host: '0.0.0.0',
     proxy: {
       "/api/": {
